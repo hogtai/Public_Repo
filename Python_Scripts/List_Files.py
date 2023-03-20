ce44311c21ff9ba80f82a62b16b6aca03246c382
Tait Hoglund <119272956+hogtai@users.noreply.github.com>
2023-03-20 10:31:20 -0500

    Update List_Files.py

--- Python_Scripts/List_Files.py
+++ Python_Scripts/List_Files.py
@@ -1,29 +1,27 @@
-# module gets information about each file in the working directory
-
 import os 
 
-#creates an empty list
+def get_file_info(path=None):
+    if path is None:
+        path = os.getcwd()
 
-files = [] 
+    files = {} 
 
-# loops for each file in present working directory
+    for root, dirs, file_names in os.walk(path):
+        folder_files = {}
+        for file_name in file_names:
+            file_info = {
+                "name": os.path.join(root, file_name),
+                "size": os.path.getsize(os.path.join(root, file_name))
+            }
+            folder_files[file_name] = file_info
+        files[root] = folder_files
 
-for file_name in os.listdir(os.getcwd()): 
+    return files
 
-#assigns variable "file_info" as a dictionary with key values
-
-    file_info = {
-        "name": file_name,
-        "size": os.path.getsize(file_name),
-    }
-
-#adds dictionary "file info" to the empty list "files"
-
-    files.append(file_info)
-
-# loops a print to screen for each dictionary in the "files" list with the file name and size
-
-for file in files:
-    print("Name:", file["name"])
-    print("Size:", file["size"], "bytes")
-    print("")
+file_structure = get_file_info()
+for folder, folder_files in file_structure.items():
+    print("Folder:", folder)
+    for file_name, file_info in folder_files.items():
+        print(" Name:", file_info["name"])
+        print(" Size:", file_info["size"], "bytes")
+        print("")
