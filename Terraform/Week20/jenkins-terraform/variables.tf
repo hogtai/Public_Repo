074b2685cad92874326e47bb0379c6565eff68c1
Tait Hoglund <hogtai@gmail.com>
2023-03-30 03:42:50 +0000

    daily work

---
+++ Terraform/Week20/jenkins-terraform/variables.tf
@@ -0,0 +1,10 @@
+variable "aws_region" {
+  type    = string
+  default = "us-east-2" # Replace with the appropriate region for your use case
+}
+variable "my_ip" {
+  type    = string
+  default = "0.0.0.0/0" # Replace with the appropriate IP address or CIDR block for your use case
+}
+
+
