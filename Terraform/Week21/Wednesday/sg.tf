7ebf83c850be0e768d442cad3ef9dc6a2d498582
Tait Hoglund <hogtai@gmail.com>
2023-04-06 03:25:28 +0000

    gettin' bread.

---
+++ Terraform/Week21/Wednesday/sg.tf
@@ -0,0 +1,37 @@
+resource "aws_security_group" "project21_webtier_sg" {
+  name        = "project21_webtier_sg"
+  description = "Allows public access to website"
+  vpc_id      = aws_vpc.project_21_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "project21_webtier_sg"
+  }
+}
