1ef49f1c1440123b896de70604a2e858af818b08
Tait Hoglund <hogtai@gmail.com>
2023-04-15 02:43:40 +0000

    work.

--- Terraform/Week22/webtier_launchtempalte.tf
+++ Terraform/Week22/webtier_launchtempalte.tf
@@ -1,3 +1,46 @@
+locals {
+  launch_template_user_data = <<-EOF
+#!/bin/bash
+yum update -y
+amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2
+yum install -y httpd
+systemctl start httpd
+systemctl enable httpd
+wget https://wordpress.org/latest.tar.gz
+tar -xzf latest.tar.gz
+cp -R wordpress/* /var/www/html/
+chown -R apache:apache /var/www/html/
+chmod -R 755 /var/www/html/
+# Update Apache configuration to use index.php as the default index file
+echo 'DirectoryIndex index.php index.html' > /etc/httpd/conf.d/wordpress.conf
+systemctl restart httpd
+
+# Create the WordPress configuration file with the database details
+cat > /var/www/html/wp-config.php <<EOT
+<?php
+define( 'DB_NAME', '${var.db_name}' );
+define( 'DB_USER', '${var.db_master_username}' );
+define( 'DB_PASSWORD', '${var.db_master_password}' );
+define( 'DB_HOST', '${aws_rds_cluster.aurora_mysql_cluster.endpoint}' );
+define( 'DB_CHARSET', 'utf8' );
+define( 'DB_COLLATE', '' );
+EOT
+
+# Add the salt values to the WordPress configuration file
+curl -s https://api.wordpress.org/secret-key/1.1/salt/ >> /var/www/html/wp-config.php
+
+# Finish the WordPress configuration file
+cat >> /var/www/html/wp-config.php <<EOT
+\$table_prefix = 'wp_';
+define( 'WP_DEBUG', false );
+if ( ! defined( 'ABSPATH' ) ) {
+    define( 'ABSPATH', dirname( __FILE__ ) . '/' );
+}
+require_once( ABSPATH . 'wp-settings.php' );
+EOT
+EOF
+}
+
 resource "aws_launch_template" "WebServerTemplate" {
   name        = var.launch_template_name
   description = var.launch_template_description
@@ -14,5 +57,5 @@ resource "aws_launch_template" "WebServerTemplate" {
     security_groups             = [aws_security_group.project21_webtier_sg.id]
   }
 
-  user_data = base64encode(var.launch_template_user_data)
+  user_data = base64encode(local.launch_template_user_data)
 }
