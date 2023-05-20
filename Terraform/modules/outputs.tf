80929d1ab2befd2e8ad0291bfd7cd336ea454017
Tait Hoglund <119272956+hogtai@users.noreply.github.com>
2023-05-19 22:29:09 -0500

    Create outputs.tf

---
+++ Terraform/modules/outputs.tf
@@ -0,0 +1,19 @@
+output "vpc_id" {
+  description = "ID of the VPC"
+  value       = module.vpc.vpc_id
+}
+
+output "ec2_instance_ids" {
+  description = "IDs of the EC2 instances"
+  value       = module.ec2.instance_ids
+}
+
+output "rds_instance_id" {
+  description = "ID of the RDS instance"
+  value       = module.rds.rds_instance_id
+}
+
+output "bastion_public_ip" {
+  description = "Public IP of the bastion host"
+  value       = module.bastion.public_ip
+}
