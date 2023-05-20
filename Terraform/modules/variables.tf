f1a9436341ebee9efdba89325d1aee90d61868d2
Tait Hoglund <119272956+hogtai@users.noreply.github.com>
2023-05-19 22:28:48 -0500

    Create variables.tf

---
+++ Terraform/modules/variables.tf
@@ -0,0 +1,40 @@
+variable "vpc_name" {
+  description = "Name of the VPC"
+  type        = string
+}
+
+variable "vpc_cidr_block" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+}
+
+variable "key_name" {
+  description = "Name of the EC2 key pair"
+  type        = string
+}
+
+variable "instance_count" {
+  description = "Number of EC2 instances to launch"
+  type        = number
+  default     = 2
+}
+
+variable "db_name" {
+  description = "Name of the RDS database"
+  type        = string
+}
+
+variable "db_username" {
+  description = "Username for the RDS database"
+  type        = string
+}
+
+variable "db_password" {
+  description = "Password for the RDS database"
+  type        = string
+}
