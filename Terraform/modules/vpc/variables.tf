cf2bff0222ba3692736fe84e2eec81b80070a505
Tait Hoglund <119272956+hogtai@users.noreply.github.com>
2023-05-19 22:32:19 -0500

    Create variables.tf
    variables.tf specifies the input variables needed for the VPC module, such as the VPC name (vpc_name) and CIDR block (vpc_cidr_block).

---
+++ Terraform/modules/vpc/variables.tf
@@ -0,0 +1,9 @@
+variable "vpc_name" {
+  description = "Name of the VPC"
+  type        = string
+}
+
+variable "vpc_cidr_block" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
