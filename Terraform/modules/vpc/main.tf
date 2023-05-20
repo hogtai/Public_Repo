0a95ca1f3147ac9b74654cc9061ed7f88e4afbd2
Tait Hoglund <119272956+hogtai@users.noreply.github.com>
2023-05-19 22:31:23 -0500

    Create main.tf
    main.tf defines AWS resources for the VPC, including the VPC itself (aws_vpc), internet gateway (aws_internet_gateway), route table (aws_route_table), and a route for internet access (aws_route).

---
+++ Terraform/modules/vpc/main.tf
@@ -0,0 +1,35 @@
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr_block
+  enable_dns_hostnames = true
+  enable_dns_support = true
+
+  tags = {
+    Name = var.vpc_name
+  }
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.vpc_name}-igw"
+  }
+}
+
+resource "aws_route_table" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.vpc_name}-rt"
+  }
+}
+
+resource "aws_route" "internet_access" {
+  route_table_id         = aws_route_table.main.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main.id
+}
+
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
