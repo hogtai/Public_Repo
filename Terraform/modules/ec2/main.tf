3fa0d1558c9227efae58c287cf84d6cddf0ad045
Tait Hoglund <119272956+hogtai@users.noreply.github.com>
2023-05-19 22:33:55 -0500

    Create main.tf
    main.tf creates EC2 instances using the aws_instance resource. The count parameter determines the number of instances to create. The instance properties, such as AMI, instance type, key name, and subnet ID, are defined using the input variables.

---
+++ Terraform/modules/ec2/main.tf
@@ -0,0 +1,15 @@
+resource "aws_instance" "ec2_instances" {
+  count         = var.count
+  ami           = var.ami
+  instance_type = var.instance_type
+  key_name      = var.key_name
+  subnet_id     = var.subnet_id
+
+  tags = {
+    Name = "${var.name_prefix}-instance-${count.index + 1}"
+  }
+}
+
+output "instance_ids" {
+  value = aws_instance.ec2_instances[*].id
+}
