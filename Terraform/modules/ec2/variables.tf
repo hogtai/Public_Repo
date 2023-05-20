b12fc16c8665e72607db3681d36fc00ffeee9c4e
Tait Hoglund <119272956+hogtai@users.noreply.github.com>
2023-05-19 22:34:47 -0500

    Create variables.tf
    variables.tf specifies the input variables required for the EC2 module, including the number of instances (count), AMI ID (ami), instance type (instance_type), key name (key_name), subnet ID (subnet_id), and name prefix (name_prefix).

---
+++ Terraform/modules/ec2/variables.tf
@@ -0,0 +1,29 @@
+variable "count" {
+  description = "Number of EC2 instances to create"
+  type        = number
+}
+
+variable "ami" {
+  description = "AMI ID for the EC2 instances"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "Instance type for the EC2 instances"
+  type        = string
+}
+
+variable "key_name" {
+  description = "Name of the EC2 key pair"
+  type        = string
+}
+
+variable "subnet_id" {
+  description = "ID of the subnet for the EC2 instances"
+  type        = string
+}
+
+variable "name_prefix" {
+  description = "Prefix for the EC2 instance names"
+  type        = string
+}
