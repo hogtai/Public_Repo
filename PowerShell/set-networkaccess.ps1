063ec96e32824203b45c88273375c17c8ca93e8b
Tait Hoglund <119272956+hogtai@users.noreply.github.com>
2023-06-16 20:52:34 -0500

    Update set-networkaccess.ps1

--- PowerShell/set-networkaccess.ps1
+++ PowerShell/set-networkaccess.ps1
@@ -1,20 +1,16 @@
 # Set 'Deny access to this computer from the network' policy
 
 # Define the accounts to be denied network access
-$accounts = @('Guests', 'Local account', 'Administrators')
+$accounts = @('Guests', 'NT AUTHORITY\Local account', 'BUILTIN\Administrators')
 
 # Loop through the accounts and set the policy
 foreach ($account in $accounts) {
-    $policyPath = "HKLM:\System\CurrentControlSet\Control\Lsa\MSV1_0\$account"
-    $existingValue = Get-ItemProperty -Path $policyPath -Name 'BackConnectionHostNames' -ErrorAction SilentlyContinue
+    $policyPath = "HKLM:\System\CurrentControlSet\Control\Lsa\MSV1_0\$account" #your path might be different
+    $existingValue = Get-ItemProperty -Path $policyPath -Name 'RestrictAnonymous'
 
-    if ($existingValue) {
-        $newValue = $existingValue.BackConnectionHostNames + ',localhost'
-    } else {
-        $newValue = 'localhost'
+    if ($existingValue.RestrictAnonymous -ne 1) {
+        Set-ItemProperty -Path $policyPath -Name 'RestrictAnonymous' -Value 1
     }
-
-    Set-ItemProperty -Path $policyPath -Name 'BackConnectionHostNames' -Value $newValue
 }
 
 # Refresh the Group Policy
